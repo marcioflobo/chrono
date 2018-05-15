0895518464e7428348187b8ec56a10dbfb469a2d
Márcio Lôbo <marcioloboti@gmail.com>
2018-05-15 15:54:47 -0300

    Criando o Cronometro
    Criação das funções javascript para disparar o relógio e pará-lo.

--- public_html/static/js/crhono.js
+++ public_html/static/js/crhono.js
@@ -1,12 +1,35 @@
-function hiddenClock(){
-    
-    var tempo = new Date();
-    var hora = tempo.getHours();
-    var minuto = tempo.getMinutes();
-    var segundo = tempo.getSeconds();
-    var relogio = hora + ":" + minuto + ":" + segundo;
-    alert("Tempo agora: "+ relogio);
-    
-    document.formCrono.cronometro.value = relogio;
-}
+   //declaração de variáveis
+            var segundo = 0 + "0";
+            var minuto = 0 + "0";
+            var hora = 0 + "0";
+            var rodar = true;
+ function tempo() {
+                //função que faz o cronometro de pausa excedida funcionar
+            
+        if (segundo == 0 && minuto == 0 && hora == 0) {
+                    parar = setInterval('tempo()', 1000);
+                }
 
+                if (segundo < 59) {
+                    segundo++;
+
+                    if (segundo < 10) {
+                        segundo = "0" + segundo;
+                    }
+                } else
+
+                if (segundo == 59 && minuto < 59) {
+
+                    segundo = 0 + "0";
+                    minuto++;
+
+                    if (minuto < 10) {
+                        minuto = "0" + minuto;
+                    }
+
+                }
+                document.formRelogio.cronometro.value = hora + ":" + minuto + ":" + segundo;
+                
+            }
+        
+        
